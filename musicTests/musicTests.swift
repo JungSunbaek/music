06b800b010f0d48c3fe487a3b7872826f130f1c3
JungSunbaek <42164538+JungSunbaek@users.noreply.github.com>
2025-06-28 18:06:52 +0900

    Initial Commit

---
+++ musicTests/musicTests.swift
@@ -0,0 +1,17 @@
+//
+//  musicTests.swift
+//  musicTests
+//
+//  Created by 정순백 on 6/28/25.
+//
+
+import Testing
+@testable import music
+
+struct musicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
